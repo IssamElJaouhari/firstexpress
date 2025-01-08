549f798504a07bf95514f0be1bc42c59a304d488
IssamElJaouhari <issamefar58@gmail.com>
2025-01-08 17:21:32 +0100

    add routes for homepage, services, contact, and 404 handling

--- server.js
+++ server.js
@@ -7,9 +7,28 @@ const port = process.env.PORT
 
 const app = express()
 
+app.get('/',(req, res)=>{
+    res.end('Hello World from homepage')
+})
 
+
+app.get('/home', (req, res)=>{
+    res.end('Hello World from second home page')
+})
+
+app.get('/services', (req, res)=>{
+    res.end('Hello World from services ')
+})
+
+app.get('/contact', (req, res)=>{
+    res.end('Hello World contact section ')
+})
+
+app.get('*', (req, res)=>{
+    res.end('404 not found ')
+})
 
 app.listen(port,()=>{
-    console.log('The server is running, ' + ' please, open your browser at http://localhost:%s',port);
+    console.log('The server is running, please, open your browser at http://localhost:%s',port);
 })
 
