8ad6e16117bfc8005e8d30be82ebc66b77ddbff4
IssamElJaouhari <issamefar58@gmail.com>
2025-01-08 17:16:48 +0100

    initialize server with Express and dotenv configuration

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+require('dotenv').config()
+
+
+const port = process.env.PORT
+
+const app = express()
+
+
+
+app.listen(port,()=>{
+    console.log('The server is running, ' + ' please, open your browser at http://localhost:%s',port);
+})
+
