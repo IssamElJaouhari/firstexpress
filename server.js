6a048f0f9750fdad8128db07eef8f744b271e7c5
IssamElJaouhari <issamefar58@gmail.com>
2025-01-09 15:26:41 +0100

    refactor server.js to improve code readability and maintainability

--- server.js
+++ server.js
@@ -2,33 +2,28 @@ const express = require('express');
 
 require('dotenv').config()
 
-
-const port = process.env.PORT
+const port = process.env.PORT || 8008
 
 const app = express()
 
-app.get('/',(req, res)=>{
-    res.end('Hello World from homepage')
-})
+// app.get("/",(req,res)=>{
+//     res.end('Hello World from index');
+// })
 
+// app.get("/home",(req,res)=>{
+//     res.end('Hello World from home page');
+// })
 
-app.get('/home', (req, res)=>{
-    res.end('Hello World from second home page')
-})
-
-app.get('/services', (req, res)=>{
-    res.end('Hello World from services ')
-})
-
-app.get('/contact', (req, res)=>{
-    res.end('Hello World contact section ')
-})
-
-app.get('*', (req, res)=>{
-    res.end('404 not found ')
-})
+// app.get("/services",(req,res)=>{
+//     res.end('Hello World from srvices page');
+// })
+// app.get("/contact",(req,res)=>{
+//     res.end('Hello World from contact');
+// })
+// app.get("*",(req,res)=>{
+//     res.end('404 not found');
+// })
 
 app.listen(port,()=>{
-    console.log('The server is running, please, open your browser at http://localhost:%s',port);
+    console.log(`The server is running,please, open your browser at http://localhost:${port}`);
 })
-
