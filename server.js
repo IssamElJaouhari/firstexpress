b1dfa941ed3d97bb3c13231a0aeaebe5a8c3c078
IssamElJaouhari <issamefar58@gmail.com>
2025-01-13 15:00:36 +0100

    refactor server.js to update view paths and remove unused first_view template

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 const express = require('express');
+const path = require('path')
+
+const app = express();
+
 require('dotenv').config();
 
 const port = process.env.PORT || 8008;
-const app = express();
 
 
 app.set('view engine', 'pug');
 
-app.set('views','./views');
+app.set('views',path.join(__dirname, 'views'));
 
-app.get("/", (req, res)=>{
-    res.render('first_view');
+// const checkavailablehours = (req, res, next) =>{
+//     const now =  new Date()
+//     const day =now.getDay()
+//     const hours = now.getHours()
+
+//     if (day === 0 || day === 6 || hours < 9 || hours >= 17) {
+//         return res.render('error',{
+//             message : "The web application is only available during working hours (Monday to Friday,  from 9 to 17)."
+//         })
+//         next()
+//     }
+// }
+
+// app.use(checkavailablehours)
+
+
+app.get('/', (req, res)=>{
+    res.render('home')
 })
 
-app.get('/first_template', (req, res)=>{
+app.get('/services', (req, res)=>{
+    res.render('services')
+})
 
-    res.render('Home');
- 
- });
-
- app.get('/contact', (req, res)=>{
-
-    res.render('contact');
- 
- });
+app.get('/contact', (req, res)=>{
+    res.render('contact')
+})
 
 
- app.get('/services', (req, res)=>{
-
-    res.render('services');
- 
- });
 
 app.listen(port, () => {
     console.log(`The server is running, please open your browser at http://localhost:${port}`);
